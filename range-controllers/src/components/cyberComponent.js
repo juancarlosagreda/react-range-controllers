a91019d2c2bcca60eae8b2442333f13c1213681d
Carias <jfcarias@tecnun.es>
2020-10-27 12:16:32 +0100

    Set initial value of sliders to 0 and added jquery

--- range-controllers/src/components/cyberComponent.js
+++ range-controllers/src/components/cyberComponent.js
@@ -7,11 +7,11 @@ class CyberComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          infoSecurity: 50,
-          vulnerabilityManage: 50,
-          infoSharing: 50,
-          training: 50,
-          businessCont: 50
+          infoSecurity: 0,
+          vulnerabilityManage: 0,
+          infoSharing: 0,
+          training: 0,
+          businessCont: 0
         }
         this.handleInfoChange = this.handleInfoChange.bind(this);
         this.handleVulnerabilityChange = this.handleVulnerabilityChange.bind(this);
@@ -59,23 +59,23 @@ class CyberComponent extends Component {
                     <Form onSubmit={this.handleSubmit}>
                         <Form.Group controlId="generalCyberResillience">
                             <Form.Control plaintext readOnly value={"Information Security Investment: " + this.state.infoSecurity + " %"}></Form.Control>
-                            <Form.Control type="range" onChange={this.handleInfoChange}/> 
+                            <Form.Control type="range" value={this.state.infoSecurity} onChange={this.handleInfoChange}/> 
                         </Form.Group>
                     <Form.Group controlId="cyberRanges">
                     <Form.Control plaintext readOnly value={"Threat and Vulnerability Management Investment: " + this.state.vulnerabilityManage + " %"}></Form.Control>
-                        <Form.Control type="range" onChange={this.handleVulnerabilityChange}/> 
+                        <Form.Control type="range" value={this.state.vulnerabilityManage} onChange={this.handleVulnerabilityChange}/> 
                     </Form.Group>
                     <Form.Group controlId="cyberRanges">
                     <Form.Control plaintext readOnly value={"Information Sharing Investment: " + this.state.infoSharing + " %"}></Form.Control>
-                        <Form.Control type="range" onChange={this.handleInfoSharingChange}/> 
+                        <Form.Control type="range" value={this.state.infoSharing} onChange={this.handleInfoSharingChange}/> 
                     </Form.Group>
                     <Form.Group controlId="cyberRanges">
                     <Form.Control plaintext readOnly value={"Training Investment: " + this.state.training + " %"}></Form.Control>
-                        <Form.Control type="range" onChange={this.handleTrainingChange}/> 
+                        <Form.Control type="range"  value={this.state.training} onChange={this.handleTrainingChange}/> 
                     </Form.Group>
                     <Form.Group controlId="cyberRanges">
                     <Form.Control plaintext readOnly value={"Business Continuity Management Investment: " + this.state.businessCont + " %"}></Form.Control>
-                        <Form.Control type="range" onChange={this.handleBusinessChange}/> 
+                        <Form.Control type="range"  value={this.state.businessCont} onChange={this.handleBusinessChange}/> 
                     </Form.Group>
                     <Button variant="primary" type="submit">
                         Submit
